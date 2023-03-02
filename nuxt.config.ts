10ed1f513bc261e69f25d6332fc706faa358baa3
mokumus <rocketpolka@gmail.com>
2023-03-02 15:15:05 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+import vuetify from 'vite-plugin-vuetify'
+
+// @ts-ignore
+export default defineNuxtConfig({
+  ssr: false,
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css',], // vuetify ships precompiled css, no need to import sass
+  vite: {
+    // @ts-ignore
+    // curently this will lead to a type error, but hopefully will be fixed soon #justBetaThings
+    ssr: {
+      noExternal: ['vuetify'], // add the vuetify vite plugin
+    },
+  },
+  modules: [
+    // @ts-ignore
+    // this adds the vuetify vite plugin
+    // also produces type errors in the current beta release
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        if (config.plugins) config.plugins.push(vuetify())
+      })
+    },
+    'nuxt-icon'
+  ],
+  
+  
+})
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  Hello Nuxt3
+  <v-card flat tile class="rounded-0" color="red" width="100%" height="1000"></v-card>
+  <v-card flat tile class="rounded-0" color="blue" width="100%" height="1000"></v-card>
+</template>
